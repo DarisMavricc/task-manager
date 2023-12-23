f40bd486e985b762d6b013c628058b62b5c6f6e4
Daris <daris@gmail.com>
2023-12-23 23:03:43 +0100

    Finished Front-Side of Web App

--- client/src/completed/completed.jsx
+++ client/src/completed/completed.jsx
@@ -1,10 +1,11 @@
-import './tasks.css'
+
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 import { ImCheckboxChecked } from "react-icons/im";
 import { TiDelete } from "react-icons/ti";
 
-export const Tasks = () => {
+
+export const Completed = () => {
 
     const addTask = () => {
         document.querySelector('.new-task').style.display = 'block';
@@ -31,67 +32,17 @@ export const Tasks = () => {
     return (
         <div className="tasks">
             <div className="create-task">
-                <h1>All Tasks</h1>
+                <h1>Completed Tasks</h1>
                 <button onClick={addTask}>+</button>
             </div>
             <div className="all-tasks">
                 <div className="task">
                     <h1>Hello World</h1>
-                    <p>Hello World hereHello World hereHello World hereHello World hereHello World hereHello World hereHello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
                     <p>Hello World here</p>
                     <p className='date'>12/3/2023</p>
                     <div className="buttons">
                         <button className='complete'>Complete</button>
                         <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete' onClick={(e) => CompleteTask(e)}>Complete</button>
-                        <button className='delete' onClick={(e) => DeleteTask(e)}><MdDelete/></button>
                         <button className='edit'><CiEdit/></button>
                     </div>
                 </div>
