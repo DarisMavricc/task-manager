0d4962cc908fad77e1556896755cbc3c9e6e8248
Daris <daris@gmail.com>
2024-01-12 00:43:23 +0100

    Finished Login and Register System

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,17 +2,31 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
-export const login = (req,res) => {
+export const login = async(req,res) => {
     const {email,password} = req.body;
     if(email,password) {
-        console.log(email,password);
-        res.status(200).json('GOOD');
+        const user = await User.find({email});
+        if(user){
+            bcrypt.compare(password,user[0].password, function(err,exist) {
+                if(exist){
+                    const data = {
+                        email: user[0].email,
+                    }
+                    const token =  jwt.sign(data,'secret',{expiresIn: '1hr'});
+                    res.cookie('accessToken',token).status(200).json('Logged in');
+                } else {
+                    res.status(401).json('Email or Password is Incorrect!');
+                }
+            })
+        }else {
+            res.status(404).json('User does not exist!');
+        }
     } else {
         res.status(401).json('All fields are mandatory');
     }
 
 
-    /* bcrypt.compare(password,newpassword, function(err,res) {
+     /* bcrypt.compare(password,newpassword, function(err,res) {
         if(res){
             console.log(res);
         } else {
@@ -38,7 +52,7 @@ export const register = async(req,res) => {
             }
             const token =  jwt.sign(data,'secret',{expiresIn: '1hr'});
             console.log('New User Created');
-            res.cookie("accessToken",token).status(200).json('User created');
+            res.cookie("accessToken",token).status(201).json('User created');
         } else {
             res.status(401).json('Error');
         }
