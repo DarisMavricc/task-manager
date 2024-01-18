2f1dcc6a1ca9bbd0e86cfa4e528c1924229d8386
Daris <daris@gmail.com>
2024-01-18 18:40:25 +0100

    Successfully connected Back-End System for Tasks with Front-End

--- client/src/incomplete/incomplete.jsx
+++ client/src/incomplete/incomplete.jsx
@@ -3,28 +3,121 @@ import { MdDelete } from "react-icons/md";
 import { ImCheckboxChecked } from "react-icons/im";
 import { TiDelete } from "react-icons/ti";
 import { Sidebar } from "../sidebar/sidebar";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/Auth";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export const Incomplete = () => {
+
+    const [title,setTitle] = useState('');
+    const [bio,setBio] = useState('');
+    const [date,setDate] = useState('');
+    const [completed,setCompleted] = useState(false);
+    const [important,setImportant] = useState(false);
+    const [err,setErr] = useState(null);
+
+    const {currentUser} = useContext(AuthContext);
+
+    const queryClient = useQueryClient();
+    
+    const {data,isLoading} = useQuery({
+        queryFn: async() => await axios.get('http://localhost:8080/api/tasks/getTasks',{params: { email: currentUser?.email }}).then(res => {return res.data}),
+        queryKey: ["tasks"],
+    });
+
+
+    const mutation = useMutation({
+        mutationFn: async(changes) => {
+          await axios.post('http://localhost:8080/api/tasks/updateTask',changes)
+        },
+        onSuccess: ()=> {
+            return queryClient.invalidateQueries(["tasks"]);
+        }
+      })
+
+      const deleteTask = useMutation({
+        mutationFn: async(id) => {
+            await axios.delete(`http://localhost:8080/api/tasks/deleteTask/${id}`);
+        },
+        onSuccess: ()=> {
+            return queryClient.invalidateQueries(["tasks"]);
+        }
+      })
+
+      const TaskCreation = useMutation({
+        mutationFn: async(task) => {
+            await axios.post("http://localhost:8080/api/tasks/addTask", task, {
+                withCredentials: true,
+            });
+        },
+        onSuccess: ()=> {
+            return queryClient.invalidateQueries(["tasks"]);
+        }
+      })
+
+
     const addTask = () => {
         document.querySelector('.new-task').style.display = 'block';
-
     }
 
     const closeTask = () => {
         document.querySelector('.new-task').style.display = 'none';
     }
 
-    async function createTask(){
-        console.log('createed');
+    const createTask = async() => {
+        if(title && bio && date){
+            const task = {
+                email: currentUser?.email,
+                name: title,
+                bio: bio,
+                date: date,
+                important: important,
+                completed: completed
+            }
+            try {
+                TaskCreation.mutate(task);
+            }catch(err){
+                console.log(err);
+            }
+            document.querySelector('.new-task').style.display = 'none';
+            document.querySelector('.task-created').style.display = 'flex';
+                setTimeout(() => {
+                    document.querySelector('.task-created').style.display = 'none';
+            }, 1500); 
+        }else {
+            setErr('You should fill all fields!');
+        }
     }
 
-    async function CompleteTask(e) {
+    const updateTask = async(e) => {
         /* e.target.style.backgroundColor = 'red';
         e.target.innerText = 'Incomplete'; */
+        const changes = {
+            id: e._id,
+        }
+
+        try {
+            mutation.mutate(changes);
+            document.querySelector('.alert').style.display = 'flex';
+            setTimeout(() => {
+                document.querySelector('.alert').style.display = 'none';
+            }, 1500); 
+        }catch(err){
+            console.log(err);
+        }
     }
 
-    async function DeleteTask(e) {
-
+    const DeleteTask = async(e) => {
+        try {
+            deleteTask.mutate(e._id);
+            document.querySelector('.task-deleted').style.display = 'flex';
+            setTimeout(() => {
+                document.querySelector('.task-deleted').style.display = 'none';
+            }, 1500); 
+        }catch(err){
+            console.log(err);
+        }
     }
 
     return (
@@ -32,59 +125,46 @@ export const Incomplete = () => {
             <Sidebar />
         <div className="tasks">
             <div className="create-task">
-                <h1>All Tasks</h1>
+                <h1>Do It Now</h1>
                 <button onClick={addTask}>+</button>
             </div>
             <div className="all-tasks">
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete'>Complete</button>
-                        <button className='delete'><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
-                <div className="task">
-                    <h1>Hello World</h1>
-                    <p>Hello World here</p>
-                    <p className='date'>12/3/2023</p>
-                    <div className="buttons">
-                        <button className='complete' onClick={(e) => CompleteTask(e)}>Complete</button>
-                        <button className='delete' onClick={(e) => DeleteTask(e)}><MdDelete/></button>
-                        <button className='edit'><CiEdit/></button>
-                    </div>
-                </div>
+            {data?.map((task) => {
+                    return (  
+                        task?.completed === false &&                  
+                        <div className="task">
+                                <h1>{task?.name}</h1>
+                                <p>{task?.bio}</p>
+                                <p className='date'>{task?.date}</p>
+                                <div className="buttons">
+                                <button onClick={() => updateTask(task)} className={task?.completed ? 'complete' : 'incomplete'}>{task?.completed ? 'Completed' : 'Incompleted'}</button>
+                                    <button onClick={() => DeleteTask(task)}className='delete'><MdDelete/></button>
+                                    <button className='edit'><CiEdit/></button>
+                                </div>
+                        </div>
+                    )
+                   
+            })}
             </div>
             <div className="new-task">
                 <div className="task-elements">
                     <button class="closeTask" onClick={closeTask}>X</button>
                     <h1>Create a task</h1>
+                    {err && <p style={{marginTop: '2px',marginBottom: '5px',color: 'red'}}>{err}</p>}<br/>
                     <label for="Title">Title</label>
-                    <input type="text" placeholder='Tittle of your task'/>
+                    <input type="text" placeholder='Tittle of your task' onChange={(e) => setTitle((e.target.value))}/>
                     <label for="Description">Description</label>
-                    <textarea class="description" placeholder='Write some description about your task'/>
+                    <textarea class="description" placeholder='Write some description about your task' onChange={(e) => setBio((e.target.value))}/>
                     <label for="Date">Date</label>
-                    <input type="date"/>
+                    <input type="date" onChange={(e) => setDate((e.target.value))}/>
                     <div className="toggle">
                         <div className="toggle-completed">
                             <p>Toggle Completed</p>
-                            <input type="checkbox" onChange={(e) => console.log(e.target.checked)}/>
+                            <input type="checkbox" onChange={(e) => setCompleted(e.target.checked)}/>
                         </div>
                         <div className="toggle-important">
                             <p>Toggle Important</p>
-                            <input type="checkbox" onChange={(e) => console.log(e.target.checked)}/>
+                            <input type="checkbox" onChange={(e) => setImportant(e.target.checked)}/>
                         </div>
                     </div>
                     <button class="create-task-btn" onClick={(e) => createTask()}>+ Create Task</button>
